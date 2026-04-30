6dbf4b3a967fb202cad259a82812d3a69c4b7c33
Christian Martinez Grau <christianmgr@ext.inditex.com>
2026-04-30 09:53:33 +0200

    [RS-X]: Add configuration for maven

--- rudo-spotless/build.gradle.kts
+++ rudo-spotless/build.gradle.kts
@@ -34,6 +34,71 @@ gradlePlugin {
     }
 }
 
+publishing {
+    // Maven Local repository for testing
+//    publications {
+//        create<MavenPublication>("rudoSpotless") {
+//            from(components["java"])
+//            groupId = "es.rudo.spotless"
+//            artifactId = "rudo-spotless-gradle-plugin"
+//            version = "1.0.0"
+//        }
+//    }
+
+    // Maven Central repository configuration
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            name.set("Rudo Spotless Gradle Plugin")
+            description.set("Spotless rules for Rudo projects.")
+            url.set("https://github.com/rudoapps/rudo-spotless-android")
+
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("rudoapps")
+                    name.set("rudo")
+                    email.set("developer@rudo.es")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:https://github.com/rudoapps/rudo-spotless-android.git")
+                developerConnection.set("scm:git:ssh://git@github.com/rudoapps/rudo-spotless-android.git")
+                url.set("https://github.com/rudoapps/rudo-spotless-android")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "staging"
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+
+    useInMemoryPgpKeys(
+        signingKeyId,
+        signingKey,
+        signingPassword,
+    )
+
+    sign(publishing.publications)
+}
+
+
 repositories {
     gradlePluginPortal()
     mavenCentral()
@@ -41,16 +106,4 @@ repositories {
 
 dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.4.0")
-}
-
-publishing {
-    // Maven Local repository for testing
-    publications {
-        create<MavenPublication>("rudoSpotless") {
-            from(components["java"])
-            groupId = "es.rudo.spotless"
-            artifactId = "rudo-spotless-gradle-plugin"
-            version = "1.0.0"
-        }
-    }
 }
